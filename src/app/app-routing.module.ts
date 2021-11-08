3ec7e0975745d9913fc1410a8b01b47dcf57a432
Brendan <brendanlucich@gmail.com>
2021-11-07 22:15:06 -0600

    Page Components and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ResumeComponent } from './resume/resume.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectsComponent } from './projects/projects.component'
+import { PassionsComponent } from './passions/passions.component';
 
 const routes: Routes = [
   { 
     path: '', 
-    component: HomeComponent 
+    component: HomeComponent, 
+    children: [
+      {
+        path: 'home',
+        component: HomePageComponent
+      },
+      {
+        path: 'about',
+        component: AboutMeComponent
+      },
+      {
+        path: 'resume',
+        component: ResumeComponent
+      },
+      {
+        path: 'projects',
+        component: ProjectsComponent
+      },      {
+        path: 'passions',
+        component: PassionsComponent
+      }
+    ]
+
   },
-  { 
-    path: 'home', 
-    component: HomeComponent,
+  {
+    path : '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PassionsComponent } from './passions/passions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutMeComponent,
+    HomePageComponent,
+    ResumeComponent,
+    ProjectsComponent,
+    PassionsComponent
   ],
   imports: [
     BrowserModule,
