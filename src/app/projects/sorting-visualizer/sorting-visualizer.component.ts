69b9a35a936dbd24d2fd76f465018a3c990dbdbb
Brendan <brendanlucich@gmail.com>
2021-11-17 17:09:04 -0600

    Bubble Sort Work

--- src/app/projects/sorting-visualizer/sorting-visualizer.component.ts
+++ src/app/projects/sorting-visualizer/sorting-visualizer.component.ts
@@ -11,21 +11,29 @@ export class SortingVisualizerComponent implements OnInit {
   ARRAY_SIZE: number = 100;
   VIS_MIN: number = 5;
   VIS_MAX: number = 650;
-
-  PRIMARY_COLOR = '#6cc3d5';
-  SECONDARY_COLOR = '#fd7e14';
-  SWAP_COLOR = '#56cc9d';
+  // PRIMARY_COLOR = 'blue';
+  // SECONDARY_COLOR = 'green';
+  // SWAP_COLOR = 'orange';
   RUN_STATUS = false;
+  NEW_STATE = false;
+  ANIMATION_SPEED_MS = 250
+  // cycle
+  // newstate
 
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit(): void {
     console.log("ngONIT")
     this.reset_array();
   }
 
+  // may need a reset on click as well for opacity
+
   reset_array() {
     console.log("reset array")
+    this.NEW_STATE = true;
+    this.RUN_STATUS = false;
     this.SORTING_ARRAY = []
     for (let i = 0; i < this.ARRAY_SIZE; ++i) {
       this.SORTING_ARRAY.push(this.get_random_int(this.VIS_MIN, this.VIS_MAX));
@@ -40,8 +48,102 @@ export class SortingVisualizerComponent implements OnInit {
     // maximum is exclusive and the minimum is inclusive
   }
 
-  animateBubbleSort() {
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+  swap(x: number, array: number[]) {
+    let temp = array[x];
+    array[x] = array[x+1];
+    array[x+1] = temp;
+  }
+
+  bubble_sort(array: number[], animations:number[][]) {
+    let arrayLength = array.length;
+    for (let i = 0; i < arrayLength - 1; i++) {
+        for (let j = 0; j < arrayLength - i - 1; j++) {
+            if (array[j] > array[j+1]) {
+                this.swap(j, array);
+                animations.push([j, j+1, 1]);
+            }
+            else animations.push([j, j+1, 0]);
+        }
+    }
+    // console.log("SORTED");
+    // console.log("1")
+    // console.log(array)
+    // console.log("2")
+    // console.log(animations)
+    return animations;
+  }
+
+  get_bubble_sort_animations(array: number[]) {
+    console.log("GET BUBBLE SORT ANIMATIONS")
+    var animations: number[][] = [];
+    if (array.length <= 1) { 
+      console.log("RETURN -1")
+      return animations;
+    }
+    animations = this.bubble_sort(array, animations);
+    console.log("RETURNING ANIMATIONS")
+    console.log(animations)
+    return animations;
+  }
+
+
+  async animateBubbleSort() {
     console.log("Bubble Sort")
+    // if numbers have been modified, return
+    if (!this.NEW_STATE) {
+      return;
+    }
+
+    this.NEW_STATE = false;
+    this.RUN_STATUS = true;
+
+    // sorted array shallow copy to allow for checking and updating of state values
+    let array_copy = Object.assign([], this.SORTING_ARRAY);
+    console.log(array_copy)
+    let animations = this.get_bubble_sort_animations(array_copy);
+    console.log(array_copy)
+    console.log(this.SORTING_ARRAY)
+
+    //animate
+    for (let i = 0; i < animations.length; i++) {
+      //user paused the animation, loop until played (is there a better way to do this?)
+      while (!this.RUN_STATUS) {
+        this.delay(10); 
+      }
+
+      let arrayBars = document.getElementsByClassName('array-bar');
+      // get indexes and styles of array values
+      let firstBar = animations[i][0]
+      let secondBar = animations[i][1]
+      let isSwap = animations[i][2]
+    
+      // highlight values being compared
+      // SECONDAY_COLOR shows comparison, SWAP_COLOR shows swap
+      arrayBars[firstBar].setAttribute("target", "_secondary")
+      arrayBars[secondBar].setAttribute("target", "_secondary")
+      await this.delay(this.ANIMATION_SPEED_MS);
+
+      // show swap if there is one
+      if (isSwap==1) {
+          arrayBars[firstBar].setAttribute("target", "_swap")
+          arrayBars[secondBar].setAttribute("target", "_swap")
+          const firstHeight = arrayBars[firstBar].getAttribute("style");
+          const secondHeight = arrayBars[secondBar].getAttribute("style");
+          arrayBars[firstBar].setAttribute("style", String(secondHeight))
+          arrayBars[secondBar].setAttribute("style", String(firstHeight))
+          await this.delay(this.ANIMATION_SPEED_MS);
+      }
+      
+      // unhighlight
+      arrayBars[firstBar].setAttribute("target", "_primary")
+      arrayBars[secondBar].setAttribute("target", "_primary")
+      await this.delay(this.ANIMATION_SPEED_MS);
+  }
+  // await this.setStateAsync({running: false});
   }
   
   animateInsertionSort() {
@@ -60,7 +162,6 @@ export class SortingVisualizerComponent implements OnInit {
     console.log("Toggle State")
     this.RUN_STATUS = !this.RUN_STATUS
     console.log(this.RUN_STATUS)
-
   }
 
 
